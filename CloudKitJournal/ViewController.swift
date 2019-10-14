6bb74ba737e9408d795d76787b608c40e47c3967
Austin Goetz <a.goetz09@gmail.com>
2019-10-14 13:53:38 -0600

    First Commit

---
+++ CloudKitJournal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CloudKitJournal
+//
+//  Created by Austin Goetz on 10/14/19.
+//  Copyright © 2019 Austin Goetz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
